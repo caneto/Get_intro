3fc304a4878b21439ecc2b7ea48dd93da092fe91
Carlos Alberto da Costa Pinto Neto <caneto@gmail.com>
2022-09-26 15:47:23 -0300

    Finalizando a Introdução ao Getx

--- lib/main.dart
+++ lib/main.dart
@@ -1,74 +1,115 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:getintro/value_controller.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomePage(),
+      home: const HomePage(),
     );
   }
 }
 
 class HomePage extends StatelessWidget {
-  HomePage({super.key});
-
-  final textControler = TextEditingController();
-
-  final valueController = ValueController();
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Navegação!'),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const Expanded(
+              child: Center(
+                // Apresentação de valor
+                child: Text(
+                  'Valor: ',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+            ),
+
+            // Botão para navegação
+            ElevatedButton(
+              onPressed: () async {
+                // Navigator.of(context).push(
+                //   MaterialPageRoute(
+                //     builder: (context) {
+                //       return DataScreen();
+                //     },
+                //   ),
+                // );
+                // Com navegar por nome
+                //Get.toNamed('/tela');
+
+                final result = await Get.to(() => DataScreen());
+
+                print(result);
+              },
+              child: const Text('Segunda tela'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class DataScreen extends StatelessWidget {
+  DataScreen({Key? key}) : super(key: key);
+
+  final textController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Definição de dado'),
+        centerTitle: true,
+      ),
       body: Padding(
         padding: const EdgeInsets.all(16),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            // Valor
-            GetX<ValueController>(
-              init: valueController,
-              builder: (ctrl) {
-                return Text("Valor definido: ${ctrl.definidoValue}");
+            // Campo de definição de valor
+            TextField(
+              controller: textController,
+            ),
+
+            // Espaçamento
+            const SizedBox(height: 10),
+
+            // Botão para voltar passando o valor
+            ElevatedButton(
+              onPressed: () {
+
+                final value = textController.text;
+
+                //Navigator.of(context).pop();
+                Get.back(result: value);
               },
+              child: const Text('Retornar'),
             ),
-
-            // Campo
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 32),
-              child: TextField(
-                controller: textControler,
-              ),
-            ),
-
-            // Botão
-            GetX<ValueController>(
-              init: valueController,
-              builder: (ctrl) {
-                return ctrl.isLoading.value
-                  ? const CircularProgressIndicator()
-                  : ElevatedButton(
-                      onPressed: () {
-                        String value = textControler.text;
-
-                        valueController.setValue(value);
-                      },
-                      child: const Text('Confirmar'),
-                    );
-                  },
-                ),
           ],
         ),
       ),
